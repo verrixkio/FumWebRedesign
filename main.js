aa3d96e0e588576838b4faa1776473e09338927b
verrixkio <verrixkio@gmail.com>
2021-04-06 17:34:00 -0600

    initial flickity

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // Jquery if you want it!
 $(document).ready(function() {
 
-    
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    // options
+    cellAlign: 'left',
+    freeScroll: true,
+    prevNextButtons: false,
+    pageDots: false,
+  });
 });
     
     
