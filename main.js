b3b701f0b0f855a248bef67051f99078f2648466
verrixkio <verrixkio@gmail.com>
2021-04-07 15:46:26 -0600

    Add all svg to front of multi step form

--- main.js
+++ main.js
@@ -14,19 +14,9 @@ $(document).ready(function() {
   var obj = {"video": {
     "value": '<iframe src="https://www.youtube.com/embed/N9rurWftUmE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     }}
-    
-  // Who We Are Tabbed 
-  // Sudo Code:
-    // Click the tab
-    // Background color should change.
-    // Border-Radius of buttons should change.
-    // Image should change.
-    // Text should change.
-    // Update Top section.
-    // Link should change.
 
   $( ".first-tab" ).click(function() {
-    
+    // Video update
     $(".dynamic-container").hide()
     $(".dynamic-img").show();
     // CSS
@@ -38,11 +28,10 @@ $(document).ready(function() {
     $(".dynamic-img").attr("src", "images/M4kkids.png");
     $('.paragraph-section .bold-text').text("M4K Orphanage - Füm™ Partnership")
     $('.paragraph-section .tab-paragraph').html("Each purchase of a Füm provides 6 orphans with a healthy meal for the day, and we look to increase that.<br><br>Together we have a positive impact.")
-
   });
 
   $( ".second-tab" ).click(function() {
-
+    // Video Update
     $(".dynamic-container").hide()
     $(".dynamic-img").show();
     // CSS
@@ -50,7 +39,6 @@ $(document).ready(function() {
     $(".first-tab").css("border-radius", "10px 0px 10px 0px")
     $(".second-tab").css("border-radius", "0px 0px 0px 0px")
     $(".third-tab").css("border-radius", "0px 10px 0px 10px")
-
     // Text
     $(".dynamic-img").attr("src", "images/core4pic.png");
     $('.paragraph-section .bold-text').text("Two sets of brothers, one idea…")
@@ -58,27 +46,19 @@ $(document).ready(function() {
   });
 
   $( ".third-tab" ).click(function() {
-
+    // On click - hide image
+    $(".dynamic-img").hide();
+    $(".dynamic-container").show()
+    $(".dynamic-container").html(obj.video.value)
     // CSS
     $(".purpose-section .image-container").css("background-color", "#9FA7A7")
     $(".first-tab").css("border-radius", "10px 0px 0px 0px")
     $(".second-tab").css("border-radius", "0px 0px 10px 0px")
     $(".third-tab").css("border-radius", "0px 10px 0px 0px")
-
     // Text
-    // On click - hide image
-    $(".dynamic-img").hide();
-    $(".dynamic-container").show()
-    $(".dynamic-container").html(obj.video.value)
-
-
     $('.paragraph-section .bold-text').text("Create Positive Habits")
     $('.paragraph-section .tab-paragraph').html("We live in a world where it is easy to make destructive habits, but hard to make positive habits.<br><br>We are here to help people create positive habits through natural solutions to lead to better lives, in a better world.”")
   });
-
-
-
-
 });
     
     
