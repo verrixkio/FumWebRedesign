f05274476dca29a36d586155da828279bde0a7b6
verrixkio <verrixkio@gmail.com>
2021-04-07 18:15:31 -0600

    first page of multi-step form working

--- main.js
+++ main.js
@@ -59,6 +59,122 @@ $(document).ready(function() {
     $('.paragraph-section .bold-text').text("Create Positive Habits")
     $('.paragraph-section .tab-paragraph').html("We live in a world where it is easy to make destructive habits, but hard to make positive habits.<br><br>We are here to help people create positive habits through natural solutions to lead to better lives, in a better world.”")
   });
+
+  // Start of Multi-Step Form
+  let form = document.getElementById('signup');
+  let name = form.elements[0];
+  
+  $( "#submit" ).click(function() {
+    console.log(name.value)
+  })
+
+  $(".multi-button").click(function() {
+    $(".multi-button").css("filter", "opacity(1)")
+    form.elements[0].value = $(this).parent().attr("id")
+    console.log(form.elements[0].value)
+    $(this).css("filter", "opacity(0.5)");
+
+  })
+
+  //Setup Multi Step Form
+  //Make each img a clickable button.
+
+
+
+
+  // SVG styling
+  var base_color = "rgb(230,230,230)";
+  var active_color = "rgb(164,181,181)";
+
+  var child = 1;
+  var length = $("section").length - 1;
+  $("#prev").addClass("disabled");
+  $("#submit").addClass("disabled");
+
+  $("section").not("section:nth-of-type(1)").hide();
+  $("section").not("section:nth-of-type(1)").css('transform','translateX(100px)');
+
+  var svgWidth = length * 200 + 24;
+  $("#svg_wrap").html(
+    '<svg version="1.1" id="svg_form_time" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 ' +
+      svgWidth +
+      ' 24" xml:space="preserve"></svg>'
+  );
+
+  function makeSVG(tag, attrs) {
+    var el = document.createElementNS("http://www.w3.org/2000/svg", tag);
+    for (var k in attrs) el.setAttribute(k, attrs[k]);
+    return el;
+  }
+
+  for (i = 0; i < length; i++) {
+    var positionX = 12 + i * 200;
+    var rect = makeSVG("rect", { x: positionX, y: 9, width: 200, height: 6 });
+    document.getElementById("svg_form_time").appendChild(rect);
+    // <g><rect x="12" y="9" width="200" height="6"></rect></g>'
+    var circle = makeSVG("circle", {
+      cx: positionX,
+      cy: 12,
+      r: 12,
+      width: positionX,
+      height: 6
+    });
+    document.getElementById("svg_form_time").appendChild(circle);
+  }
+
+  var circle = makeSVG("circle", {
+    cx: positionX + 200,
+    cy: 12,
+    r: 12,
+    width: positionX,
+    height: 6
+  });
+  document.getElementById("svg_form_time").appendChild(circle);
+
+  $('#svg_form_time rect').css('fill',base_color);
+  $('#svg_form_time circle').css('fill',base_color);
+  $("circle:nth-of-type(1)").css("fill", active_color);
+
+  
+  $(".button").click(function () {
+    $("#svg_form_time rect").css("fill", active_color);
+    $("#svg_form_time circle").css("fill", active_color);
+    var id = $(this).attr("id");
+    if (id == "next") {
+      $("#prev").removeClass("disabled");
+      if (child >= length) {
+        $(this).addClass("disabled");
+        $('#submit').removeClass("disabled");
+      }
+      if (child <= length) {
+        child++;
+      }
+    } else if (id == "prev") {
+      $("#next").removeClass("disabled");
+      $('#submit').addClass("disabled");
+      if (child <= 2) {
+        $(this).addClass("disabled");
+      }
+      if (child > 1) {
+        child--;
+      }
+    }
+    var circle_child = child + 1;
+    $("#svg_form_time rect:nth-of-type(n + " + child + ")").css(
+      "fill",
+      base_color
+    );
+    $("#svg_form_time circle:nth-of-type(n + " + circle_child + ")").css(
+      "fill",
+      base_color
+    );
+    var currentSection = $("section:nth-of-type(" + child + ")");
+    currentSection.fadeIn();
+    currentSection.css('transform','translateX(0)');
+  currentSection.prevAll('section').css('transform','translateX(-100px)');
+    currentSection.nextAll('section').css('transform','translateX(100px)');
+    $('section').not(currentSection).hide();
+  });
 });
     
     
